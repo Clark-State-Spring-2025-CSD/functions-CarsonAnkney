5cd03f891d28a3b050bc557b8c0f7f2a1e702c9d
CarsonAnkney <ankneycarson@gmail.com>
2025-05-08 16:02:19 -0400

    Update addnumbers.py

--- addnumbers.py
+++ addnumbers.py
@@ -1,3 +1,18 @@
 #This is a teacher led demo.
 #Watch this video or follow along in class.
 
+def addtwonumbers(number1, number2):
+    result = number1 + number2
+    return result
+
+def printhelloten():
+    for _ in range(10):
+        print("hello")
+x = 5
+y = 10
+
+result = addtwonumbers(x,y)
+
+print(result)
+
+printhelloten()
